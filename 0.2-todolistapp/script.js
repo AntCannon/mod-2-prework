36602e31ded4380f6ac0797ab4d35c26aee9dcb8
AntCannon <anthonycannonier@gmail.com>
2024-02-21 16:51:29 -0500

    add save/load data to/from local storage

--- 0.2-todolistapp/script.js
+++ 0.2-todolistapp/script.js
@@ -14,12 +14,26 @@ function addTask(){
   }
 
   inputBox.value = '';
+  saveData();
 }
 
 listContainer.addEventListener("click", function(e) { // listen for click in listContainer
   if (e.target.tagName === "LI") { // if LI tag was clicked
     e.target.classList.toggle("checked"); // toggle the class checked or blank
+    saveData();
   } else if (e.target.tagName === "SPAN") { // if SPAN tag was clicked
     e.target.parentElement.remove(); // delete the parent element (LI)
+    saveData();
   }
 }, false);
+
+// add local storage
+function saveData() {
+  localStorage.setItem("data", listContainer.innerHTML);
+}
+
+function loadData() {
+  listContainer.innerHTML = localStorage.getItem("data");
+}
+
+loadData();
